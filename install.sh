29ba5b9773a8d2d4ae5606963255dc35597f20b4
J Evans <10482250+jayeve@users.noreply.github.com>
2022-10-05 01:00:57 -0700

    fixup! more fixes

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ vim +PlugInstall +qall
 # zsh configs + install
 chsh -s $(which zsh)
 eval $(ssh-agent -s)
+rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp .zshrc "$HOME/.zshrc"
