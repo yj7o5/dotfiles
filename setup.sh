e67e9931f1f92da006d04ca1bc92824bf8e9c962
J Evans <10482250+jayeve@users.noreply.github.com>
2022-10-06 03:32:02 -0700

    begin tmux improvements

--- setup.sh
+++ setup.sh
@@ -17,9 +17,6 @@ cp vim-configs/plugin_configs.vim $HOME/.vim/
 cp vim-configs/vimrc $HOME/.vimrc
 vim +PlugInstall +qall
 
-# tmux
-cp .tmux.conf "$HOME/.tmux.conf"
-
 # ripgrep
 RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
 curl -Lo ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep_${RIPGREP_VERSION}_amd64.deb"
@@ -43,13 +40,7 @@ cp j.zsh-theme $HOME/.oh-my-zsh/themes/j.zsh-theme
 cp .zshrc "$HOME/.zshrc"
 cp .zsh_aliases "$HOME/.zsh_aliases"
 
-# install antigen
-#source /usr/share/zsh-antigen/antigen.zsh
-#antigen use oh-my-zsh
-#antigen bundle git
-#antigen bundle composer
-#antigen bundle zsh-users/zsh-syntax-highlighting
-#antigen theme agnoster #you need to install powerline fonts for this theme (apt-get install powerline)
-#antigen apply
+# tmux
+cp .tmux.conf "$HOME/.tmux.conf"
 
 source "$HOME/.zshrc"
